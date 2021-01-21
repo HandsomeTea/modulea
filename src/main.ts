489bcbf29a4991cd309abcadc40ff15eb6ecf3ce
haifeng_liu@bizconf.cn <haifeng_liu@bizconf.cn>
2021-01-21 17:12:50 +0800

    code update

--- src/main.ts
+++ src/main.ts
@@ -21,10 +21,20 @@ Vue.config.warnHandler = (msg: string /*, vm, trace*/) => {
     console.error(msg); /* eslint-disable-line no-console */
 };
 
+interface QiankunProps {
+    container: HTMLElement
+    // eslint-disable-next-line no-unused-vars
+    onGlobalStateChange: (callback: (state: Record<string, unknown>) => void) => void
+    // eslint-disable-next-line no-unused-vars
+    setGlobalState: (state: Record<string, unknown>) => void
+    platform: 'phone' | 'ipad' | 'spc' | 'pc'
+    lang: 'zh-cn' | 'zh-tw' | 'en'
+}
+
 let instance: null | Vue = null;
 
 // eslint-disable-next-line no-unused-vars
-const render = (props?: { container: HTMLElement, name: string, onGlobalStateChange: (callback: (state: Record<string, unknown>) => void) => void, setGlobalState: (state: Record<string, unknown>) => void }) => {
+const render = (props?: QiankunProps) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-ignore
     if (!instance) {
@@ -38,13 +48,6 @@ const render = (props?: { container: HTMLElement, name: string, onGlobalStateCha
                     return store.state.moduleOperate;
                 }
             }
-            // watch: {
-            //     noticeMasterParams() {
-            //         console.log(this.noticeMasterParams);
-            //         console.log(props?.setGlobalState);
-            //         // props?.setGlobalState({ action: this.noticeMasterParams });
-            //     }
-            // }
         }).$mount(props?.container ? props?.container.querySelector('#appA') || '#appA' : '#appA');
     }
 };
@@ -58,20 +61,28 @@ export async function bootstrap(): Promise<void> {
 }
 
 // eslint-disable-next-line no-unused-vars
-export async function mount(props: { container: HTMLElement, name: string, onGlobalStateChange: (callback: (state: Record<string, unknown>) => void) => void, setGlobalState: (state: Record<string, unknown>) => void }): Promise<void> {
-    render(props);
-    // development模式下props未传递
-    if (process?.env?.NODE_ENV === 'development') {
-        instance?.$watch('noticeMasterParams', () => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            // eslint-disable-next-line no-console
-            console.log('module a 将通信抛出数据，通知主应用做返回操作', instance.noticeMasterParams);
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            props.setGlobalState({ action: instance.noticeMasterParams });
-        });
+export async function mount(props: QiankunProps): Promise<void> {
+    if (!props.platform) {
+        return Tips.error('screen type is required.');
     }
+    store.dispatch('setScreenType', props.platform);
+
+    if (!props.lang) {
+        return Tips.error('language type is required.');
+    }
+    store.dispatch('setLanguage', props.lang);
+
+    render(props);
+
+    instance?.$watch('noticeMasterParams', () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line no-console
+        console.log('module a 将通信抛出数据，通知主应用做返回操作', instance.noticeMasterParams);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        props.setGlobalState({ action: instance.noticeMasterParams });
+    });
 }
 
 export async function unmount(): Promise<void> {
@@ -81,8 +92,4 @@ export async function unmount(): Promise<void> {
     instance = null;
     // eslint-disable-next-line no-console
     console.log('module a 卸载');
-}
-
-export async function update(): Promise<void> {
-    //
 }
